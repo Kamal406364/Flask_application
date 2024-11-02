da535eb61c9f84edfe1c58dee70aec9481377327
Kamal406364 <vimalkamal406@gmail.com>
2024-11-02 16:05:06 +0530

    Sixth

--- decorator.py
+++ decorator.py
@@ -8,7 +8,13 @@ user_db={
 }
 
 def login(username,pass_word):
-    if username in user_db:
+    global user_db
+    # print(user_db[username]['password'])
+    print(user_db.keys())
+    keys_list = list(user_db.keys())
+    print(username in keys_list)
+    if username in user_db.keys():
+        print(user_db[username]['password'])
         if user_db[username]['password']==pass_word:
             return True
         else:
@@ -20,19 +26,32 @@ def login(username,pass_word):
 
 
 def signin(func):
-    def wrapper():
-        username = input("Enter your username: ")
-        password = input("Enter your password: ")
+    def wrapper(*args,**kwargs):
+        print("Args",*args)
+        print("Kargs",type(args))
+
+        username = args[0]
+        password = args[1]
+        
+        
+
         if login(username, password):
             print("login successful")
-            return func
+            return func(username, password)
+            
         else:
             print("login failed")
     return  wrapper
 
 @signin
-def login_page():
+def login_page(username, password):
     pass
-login_page()
+
+def main():
+    username = input("Enter your username: ")
+    password = input("Enter your password: ")
+    login_page(username, password)
+
+main()
         
 
